9f10e11875003d667ddc3fe0d392c7904386266d
ChenzhouYu <2445778057@qq.com>
2024-04-10 18:29:48 +0800

    config vulkan in cmakelists.txt

--- OpenGLStudy/Time.h
+++ OpenGLStudy/Time.h
@@ -4,10 +4,10 @@ class Time
 {
 public:
     static void Init();
-    static float GetDeltaTime(); // 获取两帧之间的时间差
-    static void Update(); // 在每帧开始时调用以更新时间
+    static float GetDeltaTime(); 
+    static void Update(); 
 
 private:
     static float lastFrameTime;
-    static float deltaTime; // 当前帧与上一帧之间的时间差
+    static float deltaTime; 
 };
